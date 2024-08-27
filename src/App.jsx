50db4ea64e76d55bfa56ba4b0abbc5564632487b
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-08-27 15:45:22 +0300

    id

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
-import {
-  Routes,
-  Route,
-  Navigate,
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import ContactList from './components/ContactList/ContactList';
 import Navigation from './components/Navigation/Navigation';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import UserMenu from './components/UserMenu/UserMenu';
-
 const App = () => {
   return (
-    <Router basename="/goit-react-hw-08-phonebook">
-      <div>
-        <Navigation />
-        <UserMenu />
-        <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute>
-                <ContactList />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Navigation />
+      <UserMenu />
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute>
+              <ContactList />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
   );
 };
 
