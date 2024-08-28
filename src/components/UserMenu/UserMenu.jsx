3cff6f894f644b7b7ebe5b8fe9ca2d19aeda8b8e
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-08-28 15:22:24 +0300

    x

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../authSlice';
+import { useNavigate } from 'react-router-dom';
 import styles from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userEmail = useSelector(state => state.auth.user?.email);
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
   const handleLogout = () => {
     dispatch(logout());
-    window.location.href = '/login';
+    navigate('/login'); 
   };
 
   if (!isAuthenticated) {
